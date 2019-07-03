1ead864e4486dbcb0d58426150cc7c3dafbccdbb
Budi Darmawan <vbudi@us.ibm.com>
2019-07-02 20:34:40 -0500

    mod result

--- migrate/writeout.sh
+++ migrate/writeout.sh
@@ -34,12 +34,12 @@ do
       echo "<XMP>oc login \${SERVER}</XMP>"
       echo "<LI>Create application from the deploy template"
       echo "<XMP>oc new-app -f deploy-openshift/deploy-template.yaml -pTARGET_REPO=\${REPOHOST} -pTARGET_WORKSPACE=\${REPOSPACE}</XMP>"
+      echo "<LI>Application can be accessed from the route that you established in deploy-template object"
     else
       if [[ "$TGTTYPE" == "iks" ]]; then
         echo "<LI>Login to IBM Cloud"
         echo "<XMP>ibmcloud login </XMP>"
-        echo "<XMP>ibmcloud ks cluster-config \${CLUSTER}</XMP>"
-        echo "<XMP>export KUBECONFIG=<configfile></XMP>"
+        echo "<XMP>\`ibmcloud ks cluster-config \${CLUSTER} | grep KUBECONFIG\`</XMP>"
       elif [[ "$TGTTYPE" == "icp" ]]; then
         echo "<LI>Login to IBM Cloud Private"
         echo "<XMP>cloudctl login -a https://\${SERVER}</XMP>"
@@ -47,10 +47,14 @@ do
         echo "<LI>Login to your Kubernetes environment.<XMP>kubectl config set-credentials . . .</XMP>"
       fi
       echo "<LI>Create objects for kubernetes"
-      echo "<XMP>kubectl apply -f deploy-kube/deploy-kube.yaml</XMP>"
+      echo "<XMP>cat deploy-kube/deploy-kube.yaml | sed -e \"s/\\\${TARGET_REPO}/\$REPOHOST/g\" | sed -e \"s/\\\${TARGET_SPACE}/\$REPOSPACE/g\" > deploy-kube/deploy.yaml</XMP>"
+      echo "<XMP>kubectl apply -f deploy-kube/deploy.yaml</XMP>"
+
+      echo "<LI>Application can be accessed through the ingress entry or the NodePort service that you create"
     fi
   else
-    line=$(echo $line | sed -e "s/\:applname./$APPLNAME/g" | sed -e "s/\:appltype./$APPLTYPE/g" | sed -e "s,\:tgtdir.,$TGTPATH,g" | sed -e "s/\:tgttype./$TGTTYPE/g")
+    ts=$(date)
+    line=$(echo $line | sed -e "s/\:applname./$APPLNAME/g" | sed -e "s/\:appltype./$APPLTYPE/g" | sed -e "s,\:tgtdir.,$TGTPATH,g" | sed -e "s/\:tgttype./$TGTTYPE/g" | sed -e "s,\:timestamp,$ts,g" )
     echo "$line"
   fi
 done > $TGTPATH/result.html
